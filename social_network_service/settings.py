ef22a0a8219daebea91b6d3ab9932a52fd385b12
usrofgh <mailforgimail@gmail.com>
2023-04-23 20:38:58 +0300

    Add User model with changed username login to email and a function for generating name of the photos

--- social_network_service/settings.py
+++ social_network_service/settings.py
@@ -31,6 +31,9 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
+AUTH_USER_MODEL = "user.User"
+
+
 # Application definition
 
 INSTALLED_APPS = [
--- user/models.py
+++ user/models.py
@@ -1,3 +1,76 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import gettext as _
+
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+def user_image_file_path(instance: "User", filename: str) -> str:
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.username)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/users/avatars/", filename)
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=255, unique=True, null=False)
+    email = models.EmailField(_("email address"), unique=True)
+
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True, upload_to=user_image_file_path)
+
+    followings = models.ManyToManyField(
+        to="self",
+        related_name="followers",
+        symmetrical=False,
+    )
+
+    @property
+    def followings_count(self) -> int:
+        return self.followings.count()
+
+    @property
+    def followers_count(self) -> int:
+        return self.followers.count()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
