e9ad89e7917207525a52a5e946bb8ff8b10d5dd7
usrofgh <mailforgimail@gmail.com>
2023-04-23 21:22:45 +0300

    Add post endpoints & realize like/dislike

--- content/views.py
+++ content/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from content.models import Post
+from content.serializers import (
+    PostSerializer,
+    PostListSerializer,
+    PostDetailSerializer,
+)
+from user.permissions import IsAuthorOrReadOnly
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
+
+    def get_queryset(self):
+        tags = self.request.query_params.get("tags")
+        author = self.request.query_params.get("author")
+        queryset = self.queryset
+
+        if tags:
+            tags = tags.split(",")
+            for tag in tags:
+                queryset = queryset.filter(tags__tag_name=tag)
+
+        if author:
+            queryset = queryset.filter(author__username=author)
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostListSerializer
+        if self.action == "retrieve":
+            return PostDetailSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        permission_classes=[IsAuthenticated],
+    )
+    def like(self, request, pk=None):
+        me = self.request.user
+        post = self.get_object()
+        if me in post.likes.all():
+            post.likes.remove(me)
+            return Response({"status": "undo like"})
+
+        if me in post.dislikes.all():
+            post.dislikes.remove(me)
+
+        post.likes.add(me)
+        return Response({"status": "like"})
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        permission_classes=[IsAuthenticated],
+    )
+    def dislike(self, request, pk=None):
+        me = self.request.user
+        post = self.get_object()
+        if me in post.dislikes.all():
+            post.dislikes.remove(me)
+            return Response({"status": "undo dislike"})
+
+        if me in post.likes.all():
+            post.likes.remove(me)
+
+        post.dislikes.add(me)
+        return Response({"status": "dislike"})
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from content.views import PostViewSet
 from user.views import CreateUserView, TokenCreateView, UserViewSet, UserManageView
 
 router = routers.DefaultRouter()
 router.register("users", UserViewSet)
+router.register("posts", PostViewSet)
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
